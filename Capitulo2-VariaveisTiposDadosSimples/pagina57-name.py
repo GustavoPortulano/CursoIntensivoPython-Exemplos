4c5c1d361bb5ffd0c7b752c33ad735d315235038
GustavoPortulano <gfa.portulano@gmail.com>
2025-06-28 19:41:19 -0300

    Segundo commit: pagina 56

---
+++ Capitulo2-VariaveisTiposDadosSimples/pagina57-name.py
@@ -0,0 +1,21 @@
+# 28 de junho de 2025.
+# Sábado, inicio de noite fria
+
+# Combinando ou concatenando strings
+# Exemplo da página 57
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print("\n")
+# Nome completo
+print(full_name)
+# Compondo mensagens
+print("Hello, " + full_name.title() + "!")
+# Compondo uma mensagem e armazenando-a em uma variável
+menssage = "Hello, " + full_name.title() + "!"
+print(menssage)
+
+print("Executando uma tabulação com '\ t':")
+print("\tPython")
+print("\n")
