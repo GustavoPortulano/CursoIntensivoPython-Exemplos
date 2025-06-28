045b0e78119e3c000812a3766cf0e61b01407c8c
GustavoPortulano <gfa.portulano@gmail.com>
2025-06-28 19:27:45 -0300

    Primeiro commit. Página 56

---
+++ Capitulo2-VariaveisTiposDadosSimples/pagina56-name.py
@@ -0,0 +1,12 @@
+# 28 de junho de 2025.
+# Sábado, inicio de noite fria
+# Mudando para letras maiúsculas e minúsculas em uma
+# string usando métodos.
+# Exemplo da página 56.
+
+name = "ada lovalace"
+print("\n")
+print(name.title())
+print(name.upper())
+print(name.lower())
+print("\n")
