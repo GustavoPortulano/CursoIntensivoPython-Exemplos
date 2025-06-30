26ae07d81758a44642df46a4930f6a3bf7d9f2ba
GustavoPortulano <gfa.portulano@gmail.com>
2025-06-29 21:30:10 -0300

    Exemplo pagina 71

---
+++ Capitulo3-listas/pagina71.py
@@ -0,0 +1,21 @@
+# 29 de junho de 2025.
+# Domingo, noite fria.
+
+# Exemplo do capítulo 3, página 71.
+
+print("\n-----------------------------------------------")
+print("Lista de bicicletas:")
+bicycles = ['trek', 'cannodale', 'redline', 'specializes']
+print(bicycles)
+
+"""Extraindo o primeiro elemento da lista:"""
+print("Primeira bicicleta:")
+print(bicycles[0])
+print("\n-----------------------------------------------")
+"""Extraindo a segunda e a quarta bicicleta:"""
+print("Segunda e quarta bicicletas:")
+print(bicycles[1])
+print(bicycles[3])
+print("\n-----------------------------------------------")
+print("Último elemento da lista:")
+print(bicycles[-1])
